f58a3bdf21e5d9fa8be0530f7653f658561888a6
CodingNav <navleenkaurr@gmail.com>
2021-03-21 20:10:08 -0400

    Set up games win/loss mechanics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,48 @@
 $(document).ready(function () {
-
-    var randomNumber = Math.floor(Math.random() * (121 - 19 + 1) + 19);
-    $("#randomNumber").text(randomNumber);
-
-    $(".crystalButton").each(function () {
-        $(this).val(Math.floor(Math.random() * (13 - 1 + 1) + 1));
-        //or can do this.value = Math.random()
-        console.log(this);
-    });
-
+    
     var number = 0;
+    var wins = 0;
+    var losses = 0;
+    var randomNumber = Math.floor(Math.random() * (121 - 19 + 1) + 19);
+
+    function startGame() {
+        randomNumber = Math.floor(Math.random() * (121 - 19 + 1) + 19);
+        $("#randomNumber").text(randomNumber);
+
+        $(".crystalButton").each(function () {
+            $(this).val(Math.floor(Math.random() * (13 - 1 + 1) + 1));
+            //or can do this.value = Math.random()
+            console.log(this);
+        });
+
+        number = 0;
+    }
+
+    startGame();
 
     $(".crystalButton").click(function () {
+        if (number == 0) {
+            $("#winMessage").text("");
+            $("#lossMessage").text("");
+        }
+
         number = number + parseInt(this.value);
-        $("#number").text(number);
+        $("#score").text(number);
+
+        if (number == randomNumber) {
+            $("#winMessage").text("You win!");
+            wins = wins + 1;
+            $("#wins").text(wins);
+            startGame();
+        }
+        else if (number > randomNumber) {
+            $("#lossMessage").text("You lost!");
+            losses = losses + 1;
+            $("#losses").text(losses);
+            startGame();
+        }
+
     });
-
-
-
-
 
 })
 
